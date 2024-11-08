b735e439abfdc3eab91a2665c60aa4e0f78fe874
leozin90 <141828056+leozin90@users.noreply.github.com>
2024-11-08 08:46:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma lista em Python?',
+    'Estrutura que guarda múltiplos itens.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma biblioteca em Python?',
+    'Conjunto de funções e módulos prontos para uso.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um dicionário em Python?',
+    'Estrutura que armazena pares chave: valor.'
+)
